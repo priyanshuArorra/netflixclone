e25b85ee0379d1e4680ee5973b5528070eba1685
priyanshuArorra <113926973+priyanshuArorra@users.noreply.github.com>
2023-04-07 21:03:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
+import Row from "./components/row/Row";
 
 function App() {
   return (
@@ -10,6 +11,15 @@ function App() {
       {/* Header */}
       <Header />
       {/* Header */}
+      {/* Row */}
+      <Row title='Netflix Originals' movieType='netflix-originals' />
+      <Row title='Trending Now' movieType='trending-now' />
+      <Row title='Top Rated' movieType='top-rated' />
+      <Row title='Action Movies' movieType='action-movies' />
+      <Row title='Horror Movies' movieType='horror-movies' />
+      <Row title='Romance Movies' movieType='romance-movies' />
+      <Row title='Documentaries Movies' movieType='documentaries-movies' />
+      {/* End Row */}
     </div>
   );
 }
