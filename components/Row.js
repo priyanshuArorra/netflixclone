e26823364ad99941377a3bbff260aa68cf951c2d
priyanshuArorra <113926973+priyanshuArorra@users.noreply.github.com>
2023-04-07 21:02:10 +0530

    Update Row.js

--- components/Row.js
+++ components/Row.js
@@ -1,108 +1,39 @@
-function Row() {
+import { useEffect, useState } from "react";
+
+import { firebaseDatabase } from "../../firebase/firebase";
+
+function Row(props) {
+  const [movies, setMovies] = useState([]);
+
+  const { title, movieType } = props;
+
+  const leafRoot = 'movies';
+
+  useEffect(() => {
+    fetchMovies(movieType);
+  }, []);
+
+  const fetchMovies = (movieType) => {
+    const movieRef = firebaseDatabase.ref(`${leafRoot}/${movieType}`);
+    movieRef.on("value", (snapshot) => {
+      const movies = snapshot.val();
+      if (movies && movies.length !== 0) {
+        setMovies(() => movies);
+      }
+    });
+  };
+
   return (
-    <div class="row">
-      <h2>NETFLIX ORGINALS</h2>
-      <div class="row__posters">
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//9yxep7oJdkj3Pla9TD9gKflRApY.jpg"
-          alt="Jupiter's Legacy"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//4EYPN5mVIhKLfxGruy7Dy41dTVn.jpg"
-          alt="Lucifer"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//34FaY8qpjBAVysSfrJ1l7nrAQaD.jpg"
-          alt="Luis Miguel: The Series"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//mYsWyfiIMxx4HDm0Wck7oJ9ckez.jpg"
-          alt="Selena: The Series"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//o7uk5ChRt3quPIv8PcvPfzyXdMw.jpg"
-          alt="Who Killed Sara?"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//asDqvkE66EegtKJJXIRhBJPxscr.jpg"
-          alt="Love, Death &amp; Robots"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//obLBdhLxheKg8Li1qO11r2SwmYO.jpg"
-          alt="Cobra Kai"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//3NTAbAiao4JLzFQw6YxP1YZppM8.jpg"
-          alt="Elite"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg"
-          alt="Stranger Things"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//MoEKaPFHABtA1xKoOteirGaHl1.jpg"
-          alt="Money Heist"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//scZlQQYnDVlnpxFTxaIv2g0BWnL.jpg"
-          alt="The Umbrella Academy"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//Q1ZYG3kDS8iVIHOYOJ9NQmV0q7.jpg"
-          alt="Haunted: Latin America"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//sgxawbFB5Vi5OkPWQLNfl3dvkNJ.jpg"
-          alt="Lupin"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//yxMpoHO0CXP5o9gB7IfsciilQS4.jpg"
-          alt="Chilling Adventures of Sabrina"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//lqS90fU1IEHSbga7X6Gej5amBvR.jpg"
-          alt="Castlevania"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//e4aqizYQ8eeTGNZMq6WiFfqoZbz.jpg"
-          alt="Ginny &amp; Georgia"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//4Bph0hhnDH6dpc0SZIV522bLm4P.jpg"
-          alt="Sex Education"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//uBRpPFcYAYLM7V3x6x7bP3Ucumj.jpg"
-          alt="Halston"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//apbrbWs8M9lyOpJYU5WXrpFbk1Z.jpg"
-          alt="Dark"
-        />
-        <img
-          class="row__poster row__posterLarge"
-          src="https://image.tmdb.org/t/p/original//6USbtATMqFLkeafx3FV3wcGZTNo.jpg"
-          alt="The Innocent"
-        />
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster row__posterLarge"
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            alt={movie.original_name}
+          />
+        ))}
       </div>
     </div>
   );
