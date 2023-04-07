42a8d582d6da1e4c4416eda54e804c83980df5ea
priyanshuArorra <113926973+priyanshuArorra@users.noreply.github.com>
2023-04-07 20:51:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
+import Nav from "./components/nav/Nav";
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <div className="app">
       {/* Nav */}
-      <div className="nav false">
-        <img
-          className="nav__logo"
-          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
-          alt=""
-        />
-        <img
-          className="nav__avatar"
-          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
-          alt=""
-        />
-      </div>
+      <Nav />
       {/* End Nav */}
       {/* Header */}
-      <header className="banner">
-        <div className="banner__contents">
-          <h1 className="banner__title">Ginny &amp; Georgia</h1>
-          <div className="banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-          </div>
-          <h1 className="banner__description">
-            Angsty and awkward fifteen year old Ginny Miller often feels more
-            mature than her thirty year old mother, the irresistible and dynamic
-            Georgia Miller...
-          </h1>
-        </div>
-        <div className="banner--fadeBottom"></div>
-      </header>
+      <Header />
       {/* Header */}
     </div>
   );
